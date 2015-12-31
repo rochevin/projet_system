2c73899b5445095dc4086ac826ea9ec73c0992ec
rochevin <vincent.rocher@etu.univ-rouen.fr>
2015-12-31 13:47:44 +0100

    Ajout des commentaires

--- projet_v3/launcher.sh
+++ projet_v3/launcher.sh
@@ -2,119 +2,132 @@
 
 
 #Liste des fonctions
+
+##Fonction qui modifie une valeur dans la base de données
 function mv_value {
-	id=$(get_id $3)
-	rm_value ${1} ${2} ${id} ""
+	id=$(get_id $3) #On récupère l'identifiant
+	rm_value ${1} ${2} ${id} "" #On suprimme l'ancienne ligne sans prendre en compte la contrainte des clés étrangères
+	#Les opérations diffèrent en fonction de si on veut suprimmer une stratégie ou un autre type de données
+	#Puis on recréer la ligne avec les nouvelles valeurs :
 	if [ $2 = "strats" ]; then
-		add_strat ${1}
+		add_strat ${1} #On appelle la fonction add_strat qui va créer une nouvelle valeur avec le même identifiant que l'ancienne
 	else
-		value=$(remove_last_char $3)
-		value=$(print_values $value "|")
-		value=$(format_for_sql $value)
-		add_value ${1} ${2} ${id}","${value}
+		value=$(remove_last_char $3) #On suprimme le dernier caractère "|"
+		value=$(print_values $value "|") #On affiche les valeurs sans l'identifiant
+		value=$(format_for_sql $value) #On formate la chaine pour qu'elle soit acceptée par sqlite
+		add_value ${1} ${2} ${id}","${value} #Puis on appelle la fonction add_value qui va créer une nouvelle valeur avec le même identifiant que l'ancienne
 	fi
 }
 
 
-
+##Fonction qui va gérer l'interface pour ajouter un utilisateur
 function add_user {
 	value=$(yad --form --title "Ajout d'un utilisateur" --center --button="gtk-ok:0" --button="gtk-close:1" --field="Nom :" --field="Prénom :" --field="Adresse mail :")
 	rep=$?
 	#Si on appuie sur le bouton close, on quitte le programme
 	[[ $rep -eq 1 ]] && exit 0
 	if [[ -n $value ]]; then
-		add_value $1 "users" "NULL,"$(format_for_sql $(remove_last_char $value))
+		add_value $1 "users" "NULL,"$(format_for_sql $(remove_last_char $value)) #On appelle la fonction add_value qui va créer un utilisateur avec l'autoincrementation
 	fi
 }
 
+
+##Fonction qui va gérer l'interface pour ajouter un rappatriement
 function add_rapp {
 	value=$(yad --form --title "Ajout d'un rappatriement" --center --button="gtk-ok:0" --button="gtk-close:1" --field="Nom du fichier :" --field="Emplacement local ::DIR" --field="Emplacement distant :")
 	rep=$?
 	#Si on appuie sur le bouton close, on quitte le programme
 	[[ $rep -eq 1 ]] && exit 0
 	if [[ -n $value ]]; then
-		add_value $1 "rapps" "NULL,"$(format_for_sql $(remove_last_char $value))
+		add_value $1 "rapps" "NULL,"$(format_for_sql $(remove_last_char $value)) #On appelle la fonction add_value qui va créer un rappatriement avec l'autoincrementation
 	fi
 }
 
 
+##Fonction qui va gérer l'interface pour ajouter une stratégie
 function add_strat {
 
+	#On recupère le path du script qui va gérer le telechargement du fichier
 	cron_shell=$(realpath "exec_cron.sh")
 
-	user_list=$(get_value $1 "users")
-	user_list=$(echo $user_list | tr " " "!")
-	user_list=$(echo $user_list | tr "|" " ")
+	#On récupère les utilisateurs
+	user_list=$(get_value $1 "users") #Via sqlite avec la fonction get_value
+	user_list=$(echo $user_list | tr " " "!") #On remplace les espaces par des ! => séparateur dans yad
+	user_list=$(echo $user_list | tr "|" " ") #On remplace les | pour l'affichage par des espaces
 
-	rapp_list=$(get_value $1 "rapps")
-	rapp_list=$(echo $rapp_list | tr " " "!")
-	rapp_list=$(echo $rapp_list | tr "|" " ")
+	#On récupère les rappatriements
+	rapp_list=$(get_value $1 "rapps") #Via sqlite avec la fonction get_value
+	rapp_list=$(echo $rapp_list | tr " " "!") #On remplace les espaces par des ! => séparateur dans yad
+	rapp_list=$(echo $rapp_list | tr "|" " ") #On remplace les | pour l'affichage par des espaces
 
+	#On définit des periodes de façon fixe
 	period_list="Journalier!Hebdomadaire!Mensuel!Annuel"
 
-
+	#Puis on affiche l'interface
 	result=$(yad --width 500 --height 200 --center --title="Programmer un téléchargement" --button="gtk-ok:0" --button="gtk-close:1" --form --date-format='%F' --separator=';' --field="Utilisateur::cb" "$user_list" --field="Rappatriement::cb" "$rapp_list" --field="Périodicité::cb" "$period_list" --field="Date::DT" --field="Heure (format HH:mm) :")
 	rep=$?
 	#Si on appuie sur le bouton close, on quitte le programme
 	[[ $rep -eq 1 ]] && exit 0
 
-
+	#On récupère l'utilisteur sélectionné
 	user=$(echo $result | cut -f1 -d";")
 	user_id=$(echo $user | cut -f1 -d" ")
 
-
+	#On récupère le rappatriement sélectionné
 	rapp=$(echo $result | cut -f2 -d";")
 	rapp_id=$(echo $rapp | cut -f1 -d" ")
 	rapp_file=$(echo $rapp | cut -f2 -d" ")
 	rapp_local=$(echo $rapp | cut -f3 -d" ")
 	rapp_dist=$(echo $rapp | cut -f4 -d" ")
 
-
+	#On récupère la période sélectionnée
 	periodicite=$(echo $result | cut -f3 -d";")
 
-
+	#On récupère la valeur du champ date
 	date=$(echo $result | cut -f4 -d";")
-
-
+	[[ ! -n $date ]] && date="2015-01-01" #Si la date ne vaut rien, on assigne le premier jour de l'an
+	#On récupère la valeur du champ de l'heure
 	temps=$(echo $result | cut -f5 -d";")
-	[[ ! -n $temps ]] && temps="00:00"
-	heure=$(echo $temps | cut -f1 -d":")
-	min=$(echo $temps | cut -f2 -d":")
+	[[ ! -n $temps ]] && temps="00:00" #Si l'heure est vide, on fourni minuit par défaut
+	heure=$(echo $temps | cut -f1 -d":") #On récupère les heures
+	min=$(echo $temps | cut -f2 -d":") #Puis les minutes
     
     
-
-
-
-
+	#Si l'utilisateur clique sur quitter 
 	[[ $rep -eq 1 ]] && exit 0
 
-	
+	#En fonction de la période sélectionnée, on applique à date une opération différente
 	case $periodicite in
 		Hebdomadaire*)
-			date=$(date -d ${date} +%w)
-			cron_date="${min} ${heure} * * ${date}"
+			date=$(date -d ${date} +%w) #On converti la date en 0...6 pour le jour de la semaine
+			cron_date="${min} ${heure} * * ${date}" #Puis on formate pour crontab
 				;;
 		Mensuel*)
-			date=$(date -d ${date} +%d)
+			date=$(date -d ${date} +%d) #On converti la date en 1..31 pour le jour du mois
 			cron_date="${min} ${heure} ${date} * *"
 				;;
 		Journalier*)
-			date="NULL"
+			date="NULL" #On affiche NULL pour journalier
 			cron_date="${min} ${heure} * * *"
 				;;
 		Annuel*)
-			date=$(date -d ${date} "+%d %m")
+			date=$(date -d ${date} "+%d %m") #On converti la date en jour plus mois
 			cron_date="${min} ${heure} ${date} *"
 	esac
 
 	
-
+	#On formate pour insérer dans la base de données
 	strat_values=",\""$user_id"\",\""$rapp_id"\",\""$periodicite"\",\""$date"\",\""$temps"\""
+
 
 	if [[ -n $2 ]]; then
 		strat_values=$2$strat_values
 		add_value $db_name "strats" $strat_values
+		data=$(get_strat_fro_cron ${db_name} ${2}) #On récupère la query qui est dans le crontab
+		crontab -l | grep -v "${data}" | crontab #Puis on suprimme la ligne du crontab qui contient cette query
+		crontab < <(crontab -l ; echo "${cron_date} bash ${cron_shell} \"${db_name}\" \"${data}\"") #Et on remplace par la nouvelle
 	else 
+		#Si on a pas fourni d'id, on en créer une nouvelle stratégie, sans ecraser l'ancienne dans le crontab
 		strat_values="NULL"$strat_values
 		last_id=$(sqlite3 ${db_name} "INSERT INTO strats VALUES (${strat_values});SELECT last_insert_rowid();")
 		data=$(get_strat_fro_cron ${db_name} ${last_id})
@@ -162,8 +175,6 @@ function report_interface {
 	mail=$(echo $user_info | cut -f4 -d" ")
 
 
-	query_strat=";"
-	query_cron=";"
 
 	if [[ ! $period_info = "Toutes" ]]; then
 		result_strat=$(sqlite3 -header ${db_name} "SELECT rapps.file_name,rapps.local_path,rapps.dist_path,strats.periodicity,strats.date,strats.time FROM strats INNER JOIN rapps ON rapps.id = strats.id_rapp WHERE id_user=${user_id} AND periodicity=${period_info};")
@@ -173,38 +184,34 @@ function report_interface {
 		result_cron=$(sqlite3 -header ${db_name} "SELECT rapps.file_name,rapps.local_path,rapps.dist_path,cron_task.periodicity,cron_task.date,cron_task.time,cron_task.date_complete,cron_task.status FROM cron_task INNER JOIN rapps ON rapps.id = cron_task.id_rapp WHERE id_user=${user_id};")
 	fi
 
-	
+	cat $modele > $temp_modele
 
-	strat_row="#Stratégies programmées\n\n"
-	cron_row="#Stratégies terminées\n\n"
-	
+	printf "%s\n\n" "#Stratégies programmées" >> $temp_modele
 	i=1
 	if [[ -n $result_strat ]]; then
-
 		for row in $result_strat; do
-			[[ $i -eq 2 ]] && strat_row+=$(echo $row | awk -F "|" '{out = "|";for(i=1;i<=NF;i++){out=out":-:|"};print out}')  && strat_row+="\n"
-			strat_row+=$row"\|\n"
+			[[ $i -eq 2 ]] && printf "%s\n" "$(echo $row | awk -F "|" '{out = "";for(i=1;i<=NF;i++){out=out":-----:|"};print substr(out,1,length(out) -1)}')" >> $temp_modele
+			printf "%s\n" "$(echo $row | awk -F "|" '{out = "";for(i=1;i<=NF;i++){out=out$i" | "};print substr(out,1,length(out) -3)}')" >> $temp_modele
 			i=$i+1
 		done
-		strat_row+=": Stratégies programmées pour ${first_name} ${name}\n"
+		printf "\n%s\n\n" ": Stratégies programmées pour ${first_name} ${name}" >> $temp_modele
 	else
-		strat_row="Aucune stratégie prévue pour ${first_name} ${name}"
+		printf "\n%s\n\n" "Aucune stratégie prévue pour ${first_name} ${name}"  >> $temp_modele
 	fi
+
+
+	printf "%s\n\n" "#Stratégies terminées" >> $temp_modele
 	i=1
 	if [[ -n $result_cron ]]; then
 		for row in $result_cron; do
-			[[ $i -eq 2 ]] && cron_row+=$(echo $row | awk -F "|" '{out = "|";for(i=1;i<=NF;i++){out=out":-:|"};print out}') && cron_row+="\n"
-			cron_row+=$row"\|\n"
+			[[ $i -eq 2 ]] && printf "%s\n" "$(echo $row | awk -F "|" '{out = "";for(i=1;i<=NF;i++){out=out":-----:|"};print substr(out,1,length(out) -1)}')" >> $temp_modele
+			printf "%s\n" "$(echo $row | awk -F "|" '{out = "";for(i=1;i<=NF;i++){out=out$i" | "};print substr(out,1,length(out) -3)}')" >> $temp_modele
 			i=$i+1
 		done
-		cron_row+=": Stratégie terminées pour ${first_name} ${name}\n"
+		printf "\n%s\n\n" ": Stratégies terminées pour ${first_name} ${name}" >> $temp_modele
 	else
-		cron_row="Aucune stratégie terminée pour ${first_name} ${name}"
+		printf "\n%s\n\n" "Aucune stratégie terminées pour ${first_name} ${name}"  >> $temp_modele
 	fi
-
-	cat $modele > $temp_modele
-	echo -e $strat_row >> $temp_modele
-	echo -e $cron_row >> $temp_modele
 
 	sed -i -e "s%TITRE%Rapport des stratégies pour ${first_name} ${name}%g" $temp_modele
 	sed -i -e "s%DATE%$(date +%F)%g" $temp_modele
@@ -212,9 +219,6 @@ function report_interface {
 	sed -i -e "s%NAME%${name}%g" $temp_modele
 	sed -i -e "s%FIRST%${first_name}%g" $temp_modele
 	sed -i -e "s%MAIL_ADRESS%${mail}%g" $temp_modele
-	sed -i -e "s%STRAT_CONTENT%${strat_row}%g" $temp_modele
-	sed -i -e "s%CRON_CONTENT%${cron_row}%g" $temp_modele
-
 
 
 	generate_report $template $temp_modele $temp_report && yad --html --browser --uri="$temp_report" --width 800 --height 800 --center --button="gtk-save:10" --button="gtk-close:1" --title="Rapport des stratégies pour ${first_name} ${name}"
@@ -245,8 +249,8 @@ function main {
 	db_name=$directory"/gestion.db" #Nom du fichier contenant la base de données
 	modele_db=$directory"/modele_db.txt" #Nom du fichier contenant le modèle de la base de données
 	modele_md=$directory"/modele_md.txt" #Nom du fichier contenant le modèle du rapport
-	func_script=$directory"/__sql_func__.sh"
-	template=$directory"/buttondown.css"
+	func_script=$directory"/__sql_func__.sh" #Nom du fichier contenant toutes les fonctions utiles au bon fonctionnement du programme
+	template=$directory"/buttondown.css" #template utilisé pour le rapport
 
 	KEY="12345" #Clé utilisée par yad pour lier les onglets au yad principal
 
@@ -267,13 +271,13 @@ function main {
 	#On récupère les données des utilisateurs, rappatriements et stratégies en faisant une requête dans la BDD
 	##utilisateurs
 	user_data=$(get_value ${db_name} "users")
-	user_data=$( echo $user_data| tr "|" " ")
+	user_data=$( echo $user_data| tr "|" " ") #On remplace les | par des espaces pour l'integration dans yad
 	##rappatriements
 	rapp_data=$(get_value ${db_name} "rapps")
-	rapp_data=$( echo $rapp_data| tr "|" " ")
+	rapp_data=$( echo $rapp_data| tr "|" " ") #On remplace les | par des espaces pour l'integration dans yad
 	##stratégies
 	strat_data=$(get_value ${db_name} "strats")
-	strat_data=$( echo $strat_data| tr "|" " ")
+	strat_data=$( echo $strat_data| tr "|" " ") #On remplace les | par des espaces pour l'integration dans yad
 
 
 	#Onglet yad pour les utilisateurs
@@ -285,7 +289,7 @@ function main {
 
 	
 	#Onlet yad pour les stratégies
-	yad --plug=$KEY --tabnum=3 --list --editable --multiple --column="Id:HD" --column="Nom:TEXT" --column="Prénom:TEXT" --column="Adresse mail:TEXT" --column="Nom du fichier:TEXT" --column="Emplacement local:TEXT" --column="emplacement distant:TEXT" --column="Periodicité:TEXT" --column="Date:TEXT" --column="Heure:TEXT" ${strat_data} > $strat_file &
+	yad --plug=$KEY --tabnum=3 --list --editable --column="Id:HD" --column="Nom:TEXT" --column="Prénom:TEXT" --column="Adresse mail:TEXT" --column="Nom du fichier:TEXT" --column="Emplacement local:TEXT" --column="emplacement distant:TEXT" --column="Periodicité:TEXT" --column="Date:TEXT" --column="Heure:TEXT" ${strat_data} > $strat_file &
 
 	#Yad principal, regroupe les trois onglets utilisateurs,rappatriement et stratégie
 	yad --notebook --key=$KEY --tab="Utilisateurs" --tab="Rappatriements" --tab="Stratégies" --width 700 --height 500 --center --button="gtk-add:10" --button="gtk-edit:20" --button="gtk-delete:30" --button="Générer rapport!gtk-dnd:40" --button="gtk-close:1" --title="Gestion automatisée des rappatriements" --image=gnome-icon-theme --image-on-top --text="Sélections des données :"
@@ -323,46 +327,51 @@ function main {
 	#Si il ne veut pas ajouter ni générer de rapport, on prend en compte les trois types de données
 	else
 		#user n'est pas vide, c'est que l'utilisateur veut modifier/suprimmer un utilisateur
-		if [[ -n $user ]]; then
-
-			#rep=20 : modification d'un utilisateur
-			if [[ $rep -eq 20 ]]; then
-				mv_value $db_name "users" $user #On renvoit vers la fonction de modification de données
-			elif [[ $rep -eq 30 ]]; then
-				id=$(get_id $user) #On récupère l'id et on renvoit vers la fonction de suppression de données
-				rm_value $db_name "users" $id "PRAGMA foreign_keys = ON;"
-			fi
+		if [[ -n $user ]]; then #On vérifie que la variable n'est pas vide
+			for u in $user; do #On parcours chaque ligne proposé par l'utilisateur (multiligne)
+				#rep=20 : modification d'un utilisateur
+				if [[ $rep -eq 20 ]]; then
+					mv_value $db_name "users" $u #On renvoit vers la fonction de modification de données
+				#rep=30 : supression d'un utilisateur
+				elif [[ $rep -eq 30 ]]; then
+					id=$(get_id $u) #On récupère l'id et on renvoit vers la fonction de suppression de données
+					rm_value $db_name "users" $id "PRAGMA foreign_keys = ON;"
+				fi
+			done
 		fi
 
-		if [[ -n $rapp ]]; then
-
-			if [[ $rep -eq 20 ]]; then
-				mv_value $db_name "rapps" $rapp
-			elif [[ $rep -eq 30 ]]; then
-				id=$(get_id $rapp)
-				rm_value $db_name "rapps" $id "PRAGMA foreign_keys = ON;"
-			fi
+		if [[ -n $rapp ]]; then #On vérifie que la variable n'est pas vide
+			for r in $rapp; do #On parcours chaque ligne proposé par l'utilisateur (multiligne)
+				#rep=20 : modification d'un rappatriement
+				if [[ $rep -eq 20 ]]; then
+					mv_value $db_name "rapps" $r #On renvoit vers la fonction de modification de données
+				#rep=30 : supression d'un rappatriement
+				elif [[ $rep -eq 30 ]]; then
+					id=$(get_id $r) #On récupère l'id et on renvoit vers la fonction de suppression de données
+					rm_value $db_name "rapps" $id "PRAGMA foreign_keys = ON;"
+				fi
+		done
 		fi
 
-		if [[ -n $strat ]]; then
-
-			if [[ $rep -eq 20 ]]; then
-				mv_value $db_name "strats" $strat
+		if [[ -n $strat ]]; then #On vérifie que la variable n'est pas vide
+			#rep=20 : modification d'une strategie
+			if [[ $rep -eq 20 ]]; then 
+				mv_value $db_name "strats" $strat #On renvoit vers la fonction de modification de données
+			#rep=30 : supression d'une strategie
 			elif [[ $rep -eq 30 ]]; then
-				id=$(get_id $strat)
-				data=$(get_strat_fro_cron ${db_name} ${id})
-				crontab -l | grep -v "${data}" | crontab
-				rm_value $db_name "strats" $id "PRAGMA foreign_keys = ON;"
+				id=$(get_id $strat) #On récupère l'identifiant
+				data=$(get_strat_fro_cron ${db_name} ${id}) #On récupère la query qui est dans le crontab
+				crontab -l | grep -v "${data}" | crontab #Puis on suprimme la ligne du crontab qui contient cette query
+				rm_value $db_name "strats" $id "PRAGMA foreign_keys = ON;" #On récupère l'id et on renvoit vers la fonction de suppression de données
 
 			fi
 		fi
 
 		
 	fi
-	launch $launcher
+	launch $launcher #On lance le launcher après application des paramètres
 
 }
 
 #On lance la fonction main
-
 main
